5eae30573b7075db95c59f0816a18d5424c9049a
bharusha23 <77296486+bharusha23@users.noreply.github.com>
2023-04-10 08:32:02 -0400

    Add files via upload
    I commented out the code that printed out all the student schedules before. Just fixing this error

--- Main (1).java
+++ Main (1).java
@@ -1,3 +1,11 @@
+/** Senior Seminar Code - By Shanaya
+* More information in the README File
+*The problem this code is solving for is creating a schedule-conflict minimization for Senior Seminars
+* There are 5 timeslots and 5 rooms to be filled per timeslot
+* There are 18 sessions & 74 students; 16 students max per session
+* All the students have given their preferences & now a schedule must be made and students must be assigned
+**/
+
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.io.FileNotFoundException;  // Import this class to handle errors
@@ -6,11 +14,14 @@ import java.util.ArrayList;//Import ArrayList
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    SeminarMethods sm = new SeminarMethods();
-    sm.uploadStudentPrefs();
-    sm.uploadSessionDetails();
-    sm.findPopSess();
-    sm.makeSchedule();
-  }
-}
+    SeminarMethods sm = new SeminarMethods();//create a new instance of the Seminar Methods class
+    sm.uploadStudentPrefs();//upload the student preferences data
+    sm.uploadSessionDetails();//upload all the details about the Senior Seminars
+    sm.findPopSess();//reorder the session list in terms of popularity of the session
+    sm.makeSchedule();//map out the schedule based on popularity
+    sm.printSchedule();
+    sm.placeStudents();//put students into sessions based on their preferences
+    sm.printRosters();//print out the rosters of all the Sessions
+    sm.printStudentSchedule();
+  }//close public static void main
+}//close Main
